c621538fb7867b51dc3308d3812af5f4e643d1a4
ismoil <ismoiljoraxonov1@gmail.com>
2024-11-12 09:54:03 +0900

    first commit

---
+++ src/main/java/com/example/h2onlinedatabase/H2onlineDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2onlinedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2onlineDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2onlineDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/h2onlinedatabase/H2onlineDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2onlinedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2onlineDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
